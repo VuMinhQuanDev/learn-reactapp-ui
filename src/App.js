bdcb6fae2951b78d20081b7a7761805a451cb371
VuMinhQuanDev <vuminhquan2904@gmail.com>
2023-01-24 13:01:44 +0700

    dev branch

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-// import logo from './logo.svg';
-import './App.scss';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
 import React from 'react';
+import './App.scss';
+
 const App = (props) => {
   return (
     <>
-      <h1>Vũ Minh Quân</h1>
-      <MyComponent></MyComponent>
+      <div className='app-container'>Phùng Quỳnh Hương</div>
     </>
   )
 }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Vũ Minh Quân with HP
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
